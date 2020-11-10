99e3c30e034fdd68ace01177574b99ed12ae9ce5
Omar <omar841203@mail.com>
2020-11-10 09:44:25 -0600

    esta si es la ultima versión

---
+++ Cinev2/Cinev2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Cinev2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Cinev2/Cinev2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cinev2.MvcApplication" Language="C#" %>
